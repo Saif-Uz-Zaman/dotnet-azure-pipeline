248405572bfad3a9fece444c09cddad4920a355c
saif.zaman <saif.zaman@selise.ch>
2019-12-02 09:24:11 +0600

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using SimpleAPI.Controllers;
+using Xunit;
+
+namespace SimpleAPI.Test {
+    public class UnitTest1 {
+        ValuesController controller = new ValuesController ();
+        [Fact]
+        public void GetReturnsMyName () {
+            var returnValue = controller.Get (1);
+            Assert.Equal ("Something Else", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1 () {
+
+        }
+    }
+}
